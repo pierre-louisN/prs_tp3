1f94a2dfd79152d56ba097729d2ca71bf81a90f7
pierre-louisN <nohetpl@gmail.com>
2021-10-28 17:55:38 +0200

    split du main en fonction

--- client_udp.c
+++ client_udp.c
@@ -13,30 +13,35 @@
 #define PORT	 8080 // numéro de port
 #define MAXLINE 1024 // taille max des message qu'on peut recevoir 
 
+void send_udp();
+void recv_udp(); 
 
-int main() {
-	int sockfd; // socket du client
-	char buffer[MAXLINE]; // tableau pour recevoir des messages
-	struct sockaddr_in servaddr; // structure qui va contenir notre socket 
+// il faut utiliser des pointeurs pour modifier les éléments originaux
+void createsocket(int *sockfd, struct sockaddr_in *servaddr, int port){
 	// Création de la socket (descripteur de fichier )
-	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+	if ( (*sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
 		perror("socket creation failed");
 		exit(EXIT_FAILURE);
 	}
-
-	memset(&servaddr, 0, sizeof(servaddr)); // créer l'espace mémoire pour stocker l'adresse
-	
+	memset(servaddr, 0, sizeof(servaddr)); // créer l'espace mémoire pour stocker l'adresse
 	// Information du serveur 
-	servaddr.sin_family = AF_INET; //IPv4
-	servaddr.sin_port = htons(PORT); // port de la socket par laquelle on va passer
-	servaddr.sin_addr.s_addr = INADDR_ANY; // le serveur prend n'importe quelle adresse
-	
-	int n, len;
+	servaddr->sin_family = AF_INET; //IPv4
+	servaddr->sin_port = htons(port); // port de la socket par laquelle on va passer
+	servaddr->sin_addr.s_addr = INADDR_ANY; // le serveur prend n'importe quelle adresse
+}
+
+int thw_client(int sockfd, struct sockaddr_in servaddr){
+    char buffer[MAXLINE]; // tableau pour recevoir des messages
+    int n, len;
+
+	len = sizeof(servaddr);
     // début du three way hanshake
     char *message = "SYN"; // message de connexion
-    sendto(sockfd, (const char *)message, strlen(message), MSG_CONFIRM, (const struct sockaddr *) &servaddr, sizeof(servaddr));
-	printf("SYN envoyé\n");
-
+    if(sendto(sockfd, (const char *)message, strlen(message), MSG_CONFIRM, (const struct sockaddr *) &servaddr, sizeof(servaddr)) > 0){
+        printf("SYN envoyé\n");
+    }else{
+        return 0;
+    }
 	// WAITALL signifie que la socket va être bloqué tant qu'elle ne recoit pas la totalité des données
     // recvfrom retourne la taille du message en octets, 0 si on attend trop longtemps et qu'il n'y a pas de message
 	// servaddr est l'addr où on envoie les données
@@ -50,47 +55,58 @@ int main() {
     printf("'%s'\n", ptr);
     if(strcmp(ptr,"SYN-ACK")==0){
         ptr = strtok(NULL, delim); // le serveur nous donne le numéro de port de sa nouvelle socket
-        int socket_serv_data = atoi(ptr);
-        printf("data socket port : %d\n", socket_serv_data);
+        //int socket_serv_data = atoi(ptr);
+        //printf("data socket port : %d\n", socket_serv_data);
         message = "ACK"; // message de test
-        sendto(sockfd, (const char *)message, strlen(message), MSG_CONFIRM, (const struct sockaddr *) &servaddr, sizeof(servaddr));
-        printf("ACK envoyé\n");
-
-        // fin du three way handshake, on envoi des données sur le port de la socket data
-        char *filename = "bigfile.txt"; // nom du fichier qu'on souhaite avoir
-        //servaddr.sin_port = htons(ptr);
-        printf("%d %s %d\n",socket_serv_data,inet_ntoa(servaddr.sin_addr),ntohs(servaddr.sin_port));
-        servaddr.sin_port = htons(socket_serv_data); // on peut utiliser la même socket mais il faut changer le port 
-        int n2 = sendto(sockfd, (const char *)filename, strlen(filename), MSG_CONFIRM, (const struct sockaddr *) &servaddr, sizeof(servaddr));
-        if(n2<0){
-            perror("error sendto");
-        }
-        printf("filename envoyé %d\n",n2);
-        //printf("contenu du fichier %s :\n", filename);
-
-        FILE *fp;
-
-        fp = fopen("output_file.txt", "w+");
-        n=recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *) &servaddr, &len);
-        while(strcmp(buffer,"EOF")==0){ // serveur nous envoi EOF pour nous signifier qu'il a fini, pas bon pour les perfs peut être car strcmp coûte du temps
-            //buffer[n] = '\0'; // signifie la fin d'un string (ici le message)
-	        printf(" n : %d\n", n);
-            fputs(buffer, fp);
-            bzero(buffer,sizeof(buffer)); // vide le buffer
-            n=recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *) &servaddr, &len);
-        }
-        printf("Fichier reçu et crée\n");
-        fclose(fp);
-        
-        
-
-
+        if(sendto(sockfd, (const char *)message, strlen(message), MSG_CONFIRM, (const struct sockaddr *) &servaddr, sizeof(servaddr)) > 0){
+            printf("ACK envoyé\n");
+        }else{
+            return 0;
+        } 
+    }else{
+        return 0;
     }
+    return atoi(ptr);
     
-    
+}
 
+void exchange_file(int port, int sockfd, struct sockaddr_in servaddr){
+    int n, len;
+	char buffer[MAXLINE];
 
+	// fin du three way handshake, on envoi des données sur le port de la socket data
+	char *filename = "bigfile.txt"; // nom du fichier qu'on souhaite avoir
+	//servaddr.sin_port = htons(ptr);
+	//printf("%d %s %d\n",socket_serv_data,inet_ntoa(servaddr.sin_addr),ntohs(servaddr.sin_port));
+	servaddr.sin_port = htons(port); // on peut utiliser la même socket mais il faut changer le port 
+	int n2 = sendto(sockfd, (const char *)filename, strlen(filename), MSG_CONFIRM, (const struct sockaddr *) &servaddr, sizeof(servaddr));
+	printf("filename envoyé %d\n",n2);
+	//printf("contenu du fichier %s :\n", filename);
 
+	FILE *fp;
+
+	fp = fopen("output_file.txt", "w+");
+	recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *) &servaddr,&len);
+	while(strcmp(buffer,"EOF")==0){ // serveur nous envoi EOF pour nous signifier qu'il a fini, pas bon pour les perfs peut être car strcmp coûte du temps peut être
+		fputs(buffer, fp);
+		bzero(buffer,sizeof(buffer)); // vide le buffer
+		recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *) &servaddr, &len);
+	}
+	printf("Fichier reçu et crée\n");
+	fclose(fp);
+}
+
+int main() {
+	int sockfd; // socket du client
+	struct sockaddr_in servaddr; // structure qui va contenir notre socket 
+    createsocket(&sockfd,&servaddr,PORT);
+
+    int res_twh = thw_client(sockfd,servaddr);
+    if(res_twh != 0){
+        exchange_file(res_twh, sockfd, servaddr);
+    }
 	close(sockfd);
 	return 0;
 }
+
+// à faire : changer le nom des variables, définir send et recv pour les messages d'erreurs;
--- server.c
+++ server.c
@@ -13,109 +13,103 @@
 #define PORT2	 1234
 #define MAXLINE 1024
 
+void createsocket(int *sockfd, struct sockaddr_in *servaddr, int port){
+	// Creating socket file descriptor
+	if ( (*sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+		perror("socket creation failed");
+		exit(EXIT_FAILURE);
+	}
+	
+	memset(servaddr, 0, sizeof(servaddr));
+	
+	// Info du serveur 
+	servaddr->sin_family = AF_INET; // IPv4
+	servaddr->sin_addr.s_addr = INADDR_ANY; // le serveur prend cette adresse pour être joignable sur n'importe quel interface réseau
+	servaddr->sin_port = htons(port);
+	
+	// Bind de la socket sur l'adresse du serveur 
+	if ( bind(*sockfd, (const struct sockaddr *)servaddr, sizeof(*servaddr)) < 0 )
+	{
+		perror("bind failed");
+		exit(EXIT_FAILURE);
+	}
+}
+
+int exchange_file(int len, int data_socket, struct sockaddr_in data_addr){
+    char buffer[MAXLINE];
+    // entrée dans la connexion 
+    int n = recvfrom(data_socket, (char *)buffer, MAXLINE, MSG_WAITALL, ( struct sockaddr *) &data_addr, &len);
+    buffer[n] = '\0'; 
+    printf("Client : %s\n", buffer);
+    FILE *fp;
+    char str[64];
+
+    /* opening file for reading */
+    fp = fopen(buffer , "r"); // ouverture du fichier
+    if(fp == NULL) {
+        perror("Error opening file");
+        return(-1);
+    }
+    while( fgets(str, 64, fp)!=NULL ) { // fgets : str => la chaîne qui contient la chaine de caractère lu, 60 : nmbre max de caractère à lire, fr : stream d'où sont lus les chaînes
+        /* writing content to stdout */
+        //puts(str);
+        // on va écrire le texte vers le client 
+        sendto(data_socket, (const char *)str, strlen(str), MSG_CONFIRM, (const struct sockaddr *) &data_addr, len);
+        //bzero(str,sizeof(str));
+    }
+    strcpy(str,"EOF");
+    sendto(data_socket, (const char *)str, strlen(str), MSG_CONFIRM, (const struct sockaddr *) &data_addr, len);
+    fclose(fp);
+    return 1;
+}
+
+int twh_serv(int sockfd, struct sockaddr_in cliaddr){
+    int len, n;
+    char buffer[MAXLINE];
+	len = sizeof(cliaddr);//taille de la structure qui mène vers le client
+    n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, ( struct sockaddr *) &cliaddr, &len);
+	buffer[n] = '\0'; 
+	printf("Client : %s\n", buffer);
+    if((strcmp(buffer,"SYN")) == 0){ //si le client s'est bien connecté alors on crée la socket de données
+        int data_socket; //on va créer la socket de donnée
+        struct sockaddr_in data_addr;
+    
+        createsocket(&data_socket,&data_addr,PORT2);
+        
+        char message[64] = "SYN-ACK_";
+        char port[5];
+        // itoa(PORT2, port, 10);
+        sprintf(port, "%d", PORT2);
+        strcat(message,port);
+        sendto(sockfd, (const char *)message, strlen(message), MSG_CONFIRM, (const struct sockaddr *) &cliaddr, len);
+        
+        n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, ( struct sockaddr *) &cliaddr, &len);
+        buffer[n] = '\0'; 
+	    printf("Client : %s\n", buffer);
+        if(strcmp(buffer,"ACK") == 0){
+            if(exchange_file(len,data_socket,data_addr) > 0){
+                return 1;
+            }
+        }
+    }
+    return 0;
+
+}
+
+
+
 
 int main() {
 	int sockfd;
 	char buffer[MAXLINE];
 	struct sockaddr_in servaddr, cliaddr;
-	
-	// Creating socket file descriptor
-	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
-		perror("socket creation failed");
-		exit(EXIT_FAILURE);
-	}
-	
-	memset(&servaddr, 0, sizeof(servaddr));
 	memset(&cliaddr, 0, sizeof(cliaddr));
-	
-	// Info du serveur 
-	servaddr.sin_family = AF_INET; // IPv4
-	servaddr.sin_addr.s_addr = INADDR_ANY; // le serveur prend cette adresse pour être joignable sur n'importe quel interface réseau
-	servaddr.sin_port = htons(PORT);
-	
-	// Bind de la socket sur l'adresse du serveur 
-	if ( bind(sockfd, (const struct sockaddr *)&servaddr,
-			sizeof(servaddr)) < 0 )
-	{
-		perror("bind failed");
-		exit(EXIT_FAILURE);
-	}
-	
-	int len, n;
+    createsocket(&sockfd,&servaddr,PORT);
 
-	len = sizeof(cliaddr);//taille de la structure qui mène vers le client
-
-
-    n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, ( struct sockaddr *) &cliaddr, &len);
-	buffer[n] = '\0'; 
-	printf("Client : %s\n", buffer);
-    // char message[64] = "SYN_ACK";
-    // char *port = "1234";
-    // strcat(message,port);
-    
-	// sendto(sockfd, (const char *)message, strlen(message), MSG_CONFIRM, (const struct sockaddr *) &cliaddr, len);
-	// printf("SYN_ACK sent.\n");
-
-    if((strcmp(buffer,"SYN")) == 0){ //si le client s'est bien connecté alors on crée la socket de données
-        int data_socket; //on va créer la socket de donnée
-        struct sockaddr_in data_addr;
-	
-        if ( (data_socket = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
-            perror("socket creation failed");
-            exit(EXIT_FAILURE);
-        }
-        memset(&data_addr, 0, sizeof(data_addr));
-        
-        data_addr.sin_family = AF_INET; // IPv4
-        data_addr.sin_addr.s_addr = INADDR_ANY; 
-        data_addr.sin_port = htons(PORT2);
-        
-        if (bind(data_socket, (const struct sockaddr *)&data_addr,
-                sizeof(data_addr)) < 0 )
-        {
-            perror("bind failed");
-            exit(EXIT_FAILURE);
-        }else{
-            char message[64] = "SYN-ACK_";
-            char port[5];
-            // itoa(PORT2, port, 10);
-            sprintf(port, "%d", PORT2);
-            strcat(message,port);
-            sendto(sockfd, (const char *)message, strlen(message), MSG_CONFIRM, (const struct sockaddr *) &cliaddr, len);
-        }
-        n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, ( struct sockaddr *) &cliaddr, &len);
-        buffer[n] = '\0'; 
-	    printf("Client : %s\n", buffer);
-        if(strcmp(buffer,"ACK") == 0){
-            // entrée dans la connexion 
-            n = recvfrom(data_socket, (char *)buffer, MAXLINE, MSG_WAITALL, ( struct sockaddr *) &data_addr, &len);
-            buffer[n] = '\0'; 
-            printf("Client : %s\n", buffer);
-            FILE *fp;
-            char str[64];
-
-            /* opening file for reading */
-            fp = fopen(buffer , "r"); // ouverture du fichier
-            if(fp == NULL) {
-                perror("Error opening file");
-                return(-1);
-            }
-            while( fgets(str, 64, fp)!=NULL ) { // fgets : str => la chaîne qui contient la chaine de caractère lu, 60 : nmbre max de caractère à lire, fr : stream d'où sont lus les chaînes
-                /* writing content to stdout */
-                //puts(str);
-                // on va écrire le texte vers le client 
-                sendto(data_socket, (const char *)str, strlen(str), MSG_CONFIRM, (const struct sockaddr *) &data_addr, len);
-                //bzero(str,sizeof(str));
-            }
-            strcpy(str,"EOF");
-            sendto(data_socket, (const char *)str, strlen(str), MSG_CONFIRM, (const struct sockaddr *) &data_addr, len);
-            fclose(fp);
-        }
-        // n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, ( struct sockaddr *) &cliaddr, &len);
-	    // buffer[n] = '\0'; 
-        // printf("Client : %s\n", buffer);
+	int res_twh = twh_serv(sockfd,cliaddr);
+    if(res_twh != 0){
+        printf("exchange successful \n");
     }
-
     // while(1){
         
 
