a284c39815d220828f28e287813aebe0813ffef2
pierre-louisN <nohetpl@gmail.com>
2021-11-14 00:37:42 +0100

    maj 2e struct client

--- client_udp.c
+++ client_udp.c
@@ -9,13 +9,24 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
-#include <time.h>
+#include <sys/time.h>
+
 
 #define PORT	 8080 // numéro de port
 #define MAXLINE 1024 // taille max des message qu'on peut recevoir 
 
 void send_udp();
 void recv_udp(); 
+
+void init_struct(struct sockaddr_in *servaddr, int port){
+	memset(servaddr, 0, sizeof(servaddr)); // créer l'espace mémoire pour stocker l'adresse
+	// Information du serveur : le client doit connaître les infos du serveur pour s'y connecter (son adresse et son port) 
+	servaddr->sin_family = AF_INET; //IPv4
+	servaddr->sin_port = htons(port); // port de la socket par laquelle on va passer
+	//servaddr->sin_addr.s_addr = INADDR_ANY; // le serveur prend n'importe quelle adresse 
+	inet_aton("127.0.0.1", &servaddr->sin_addr);
+	// mais ici on doit donner une vraie adresse au serveur car 0.0.0.0 est une adresse non routable
+}
 
 // il faut utiliser des pointeurs pour modifier les éléments originaux
 void createsocket(int *sockfd, struct sockaddr_in *servaddr, int port){
@@ -24,11 +35,8 @@ void createsocket(int *sockfd, struct sockaddr_in *servaddr, int port){
 		perror("socket creation failed");
 		exit(EXIT_FAILURE);
 	}
-	memset(servaddr, 0, sizeof(servaddr)); // créer l'espace mémoire pour stocker l'adresse
-	// Information du serveur 
-	servaddr->sin_family = AF_INET; //IPv4
-	servaddr->sin_port = htons(port); // port de la socket par laquelle on va passer
-	servaddr->sin_addr.s_addr = INADDR_ANY; // le serveur prend n'importe quelle adresse
+	init_struct(servaddr,port);
+	 
 }
 
 int thw_client(int sockfd, struct sockaddr_in servaddr){
@@ -37,8 +45,8 @@ int thw_client(int sockfd, struct sockaddr_in servaddr){
 
 	len = sizeof(servaddr);
     // début du three way hanshake
-    char *message = "SYN"; // message de connexion
-    if(sendto(sockfd, (const char *)message, strlen(message), MSG_CONFIRM, (const struct sockaddr *) &servaddr, sizeof(servaddr)) > 0){
+    char *message_syn = "SYN"; // message de connexion
+    if(sendto(sockfd, (const char *)message_syn, strlen(message_syn), MSG_CONFIRM, (const struct sockaddr *) &servaddr, sizeof(servaddr)) > 0){
         printf("SYN envoyé\n");
     }else{
         return 0;
@@ -57,8 +65,9 @@ int thw_client(int sockfd, struct sockaddr_in servaddr){
     if(strcmp(ptr,"SYN-ACK")==0){
         ptr = strtok(NULL, delim); // le serveur nous donne le numéro de port de sa nouvelle socket
         //printf("data socket port : %d\n", atoi(ptr));
-        message = "ACK"; // message de test
-        if(sendto(sockfd, (const char *)message, strlen(message), MSG_DONTWAIT, (const struct sockaddr *) &servaddr, sizeof(servaddr)) > 0){
+        char *message_ack = "ACK"; // message de test
+		// ici il faut utiliser une autre chaîne de caractère que message car sinon memory leak
+        if(sendto(sockfd, (const char *)message_ack, strlen(message_ack), MSG_DONTWAIT, (const struct sockaddr *) &servaddr, sizeof(servaddr)) > 0){
             printf("ACK envoyé\n");
         }else{
             return 0;
@@ -70,115 +79,155 @@ int thw_client(int sockfd, struct sockaddr_in servaddr){
     
 }
 
-void exchange_file(int port, int sockfd, struct sockaddr_in servaddr){
+
+char *concat(char const*str1, char const*str2) {
+   size_t const l1 = strlen(str1) ;
+   size_t const l2 = strlen(str2) ;
+
+    char* result = malloc(l1 + l2 + 1);
+    if(!result) return result;
+    memcpy(result, str1, l1) ;
+    memcpy(result + l1, str2, l2 + 1);
+    return result;
+}
+
+void generate_ack(char **ack_char, int ack){
+	
+	*ack_char = "ACK_";
+	int i = ack;
+	while(i>0){
+		*ack_char = concat(*ack_char,"A");
+		i--;
+	}
+
+}
+
+void exchange_file(int sockfd, struct sockaddr_in servaddr, char *input_file, char *output_file){
     int n, len;
 	char buffer[MAXLINE];
 	bzero(buffer,sizeof(buffer));
 	len = sizeof(servaddr);
 
 	// fin du three way handshake, on envoi des données sur le port de la socket data
-	char *filename = "bigfile.txt"; // nom du fichier qu'on souhaite avoir
-	//servaddr.sin_port = htons(ptr);
-	//printf("%d %s %d\n",socket_serv_data,inet_ntoa(servaddr.sin_addr),ntohs(servaddr.sin_port));
-	servaddr.sin_port = htons(port); // on peut utiliser la même socket mais il faut changer le port 
+	char *filename = input_file;
+	
 	int n2 = sendto(sockfd, (const char *)filename, strlen(filename), MSG_CONFIRM, (const struct sockaddr *) &servaddr, sizeof(servaddr));
 	printf("filename envoyé \n");
-	//printf("contenu du fichier %s :\n", filename);
 
 	FILE *fp;
 
 	int ack = 1;
 	char buffer_file[1024];
 	bzero(buffer_file,sizeof(buffer_file));
-	fp = fopen("output_file.txt", "w");
-	n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *) &servaddr,&len);
+
+	fp = fopen(output_file, "w");
+	n = recvfrom(sockfd, buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *) &servaddr,&len);
 	//buffer[n] = '\0'; // signifie la fin d'un string (ici le message)
 	//printf("Server : %s\n", buffer);
-	int nbre_octets = n; // on va compter le nombre d'octet qu'on reçoit pour calculer le débit
-	char ack_char[7];
-	clock_t before = clock(); // on va utiliser un timer pour calculer le débit en divisant le temps par le nombre de paquets
-	// clock returns the number of clock ticks elapsed since the start of the program.
+	int nbre_octets = n-6; // on va compter le nombre d'octet qu'on reçoit pour calculer le débit
+	char *ack_char;
+	// clock() est une fonction complexe donc on va plutot utiliser gettimeofday()
+	struct timeval time_before;
+	struct timeval time_after;
+	gettimeofday(&time_before,NULL);
+	char data[58];
+	bzero(data,sizeof(data));
+	char char_seq[7];
+	char_seq[7] = '\0';
+	bzero(char_seq,sizeof(char_seq));
+	// le 2e argument est la timezon mais est devenu obsolète
 	while(strcmp(buffer,"EOF")!=0){ // serveur nous envoi EOF pour nous signifier qu'il a fini, pas bon pour les perfs peut être car strcmp coûte du temps peut être
-		//recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *) &servaddr, &len);
-		//n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *) &servaddr, &len);
+		printf("%d octets reçus\n",n); 
+		//puts(buffer);
 		
-		buffer[n] = '\0'; // signifie la fin d'un string (ici le message)
+		//buffer[n] = '\0'; // signifie la fin d'un string (ici le message)
 		
 		//printf("Server : %s\n", buffer);
-		char delim[] = "_";
-		char *seq_ptr = strtok(buffer, delim);
-		//printf("Serveur : '%s'\n", buffer);
-		int num_seq = atoi(seq_ptr);
-		printf("numéro de séquence reçu : %d\n", num_seq);
-		if(ack == 1 || num_seq==ack+1){
-			ack = num_seq;
-			//printf("ack : %d\n",ack);
-			sprintf(ack_char,"%6d",ack);
-			sendto(sockfd, (const char *)ack_char, strlen(ack_char), MSG_DONTWAIT, (const struct sockaddr *) &servaddr, sizeof(servaddr));
+		 // il faut que la taille de ce tableau soit > 6 sinon on a undefined behaviour pour memcpy
+		memcpy(char_seq,buffer,6);
+		printf("le numéro de séquence est %s\n",char_seq);
+		int num_seq = atoi(char_seq);
+		printf("numéro de séquence reçu : %d\n",num_seq);
+		// if(ack == 1 || num_seq==ack+1){ // si c'est un segment reçu en continu alors on l'ACK
+		// 	ack = num_seq;
+
+		// 	generate_ack(&ack_char,ack);
+		// 	//printf("ici\n");
+		// 	//printf("ack : %s\n",ack_char);
+		// 	//sprintf(ack_char,"%6d",ack);
+		// 	// on peut utiliser strlen(ack_char) car c'est forcément une chaîne de caractères
+		// 	sendto(sockfd, (const char *)ack_char, strlen(ack_char), MSG_DONTWAIT, (const struct sockaddr *) &servaddr, sizeof(servaddr));
+		// 	printf("ACK n°%d envoyé\n",ack);
+		// }
+		// dés qu'on reçoit un message, on l'ACK
+		ack = num_seq;
+		generate_ack(&ack_char,ack);
+		//printf("ici\n");
+		//printf("ack : %s\n",ack_char);
+		//sprintf(ack_char,"%6d",ack);
+		// on peut utiliser strlen(ack_char) car c'est forcément une chaîne de caractères
+		sendto(sockfd, (const char *)ack_char, strlen(ack_char), MSG_DONTWAIT, (const struct sockaddr *) &servaddr, sizeof(servaddr));
+		printf("ACK n°%d envoyé\n",ack);
+		// printf("%c\n",buffer[8]);
+		// seq_ptr = strtok(NULL, delim);
+		memcpy(data,buffer+6,n);
+		printf("data : %s\n",data);
+		if(data!=NULL){
+			// pour écrire la chaîne au bon endroit, on va se déplacer au bon endroit avec fseek()
+			
+			fseek(fp,(num_seq-1)*58,SEEK_SET);			
+			fwrite(data,1,n-6,fp); // on enlève 7 octet car les 7 premiers octets ne sont pas utiles
 			
 		}
-		seq_ptr = strtok(NULL, delim);
-		fwrite(seq_ptr,1,n-(sizeof(ack_char)),fp); // on enlève 7 octet car les 7 premiers octets ne sont pas utiles
-		//fputs(buffer, fp);
-		//printf("buffer contient : %s\n",buffer_file);
-		//strcat(buffer_file,seq_ptr);
-		//puts(seq_ptr);
+		nbre_octets +=n-6;
 		bzero(buffer,sizeof(buffer)); // vide le buffer
 		n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *) &servaddr, &len);
-		nbre_octets +=n;
-		// char message[64];
-		// sprintf(message, "%d", message_number);
-		// int n2 = sendto(sockfd, (const char *)message, strlen(message), MSG_CONFIRM, (const struct sockaddr *) &servaddr, sizeof(servaddr));
+		
+		
 	}
-	clock_t difference = clock() - before; // le nombre de tick que prend l'échange du fichier
-	//CLOCKS_PER_SEC : nombre de tick fait par le CPU en 1 sec (pour avoir le nombre de secondes)
-	long double time = ((long double)difference*1000/(long double)CLOCKS_PER_SEC);
-	printf("Durée échange fichier : %Lf millisecondes\n",time);
-	printf("Débit : %Lf octets/s\n",(long double)nbre_octets/(time/1000));
-	//fputs(buffer_file, fp);
-	//fwrite(buffer_file,1,1024,fp);
+	gettimeofday(&time_after,NULL);
+	long double time_seconds = (time_after.tv_sec - time_before.tv_sec);
+	long double time_microsecondes = abs(time_after.tv_usec - time_before.tv_usec);
+	//printf("nombre octets : %d\n",nbre_octets);
+	//printf("Durée échange fichier : %Lf secondes et %LF microsecondes\n",time_seconds,time_microsecondes);
+	printf("Débit : %Lf octets/s\n",(long double)(nbre_octets/(time_microsecondes/(1000000))));
+	printf("%d octets reçu\n",nbre_octets);
 	printf("Fichier reçu et crée\n");
 	fclose(fp);
 }
 
-// void exchange_data(int port, int sockfd, struct sockaddr_in servaddr){
-// 	int n, len;
-// 	char buffer_data[MAXLINE];
-// 	//bzero(buffer_,sizeof(buffer)); 
-// 	len = sizeof(servaddr);
-// 	servaddr.sin_port = htons(port); // on peut utiliser la même socket mais il faut changer le port 
-// 	int message_number = 1;
-// 	while(1){
-// 		sleep(2);
-// 		char message[64];
-// 		sprintf(message, "%d", message_number);
-// 		int n2 = sendto(sockfd, (const char *)message, strlen(message), MSG_CONFIRM, (const struct sockaddr *) &servaddr, sizeof(servaddr));
-// 		recvfrom(sockfd, (char *)buffer_data, MAXLINE, MSG_WAITALL, (struct sockaddr *) &servaddr, &len);	
-// 		char delim[] = "_";
-// 		char *ptr = strtok(buffer_data, delim);
-// 		printf("Serveur : '%s'\n", buffer_data);
-// 		if(strcmp(ptr,"	ACK")==0){
-// 			ptr = strtok(NULL, delim);
-// 			int num_ack = atoi(ptr);
-// 			printf("numéro de l'ACK reçu : %d\n", num_ack);
-// 			if(num_ack==message_number){
-// 				message_number++;
-// 			}
-// 		}
-// 	}
-// }
 
-int main() {
+
+int main(int argc, char **argv) {
+	char *input_file;
+	char *output_file;
+
+
+	if(argc >= 3){
+		input_file = argv[1];
+		output_file = argv[2];
+	}else{
+		input_file = "bigfile.txt";
+		output_file = "output_file.txt";
+	}
+
+	// les arguments du client sont le nom du fichier de sortie et le fichier d'entrée
 	int sockfd; // socket du client
 	struct sockaddr_in servaddr; // structure qui va contenir notre socket 
     createsocket(&sockfd,&servaddr,PORT);
 
     int res_twh = thw_client(sockfd,servaddr);
     if(res_twh != 0){
-        exchange_file(res_twh, sockfd, servaddr);
+		struct sockaddr_in servaddr_data; // structure qui va contenir notre socket 
+		init_struct(&servaddr_data, res_twh);
+        exchange_file(sockfd, servaddr_data,input_file,output_file);
 		//exchange_data(res_twh,sockfd,servaddr);
     }
 	close(sockfd);
+	
+	char buffer[100];
+	snprintf(buffer, sizeof(buffer), "cmp --silent %s %s || echo files are different ", output_file, input_file);
+	system(buffer);
 	return 0;
 }
 
--- server.c
+++ server.c
@@ -9,14 +9,32 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <signal.h>
+#include <sys/stat.h> // pour connaître la taille du fichier
+#include <math.h> // pour arrondir
 
 
 #define PORT	 8080
 #define PORT2	 1234
 #define MAXLINE 1024
 #define WINDOW 5 // taille de la fenêtre
+#define SEGMENT_SIZE 64 // taille de la fenêtre
 
-int nbr_sock = 0;
+// on va utiiliser une structure segment qui contient : 
+// - les donnes lu du fichier : le point  de départ dans le fichier et le nombre d'octet lus
+// - son numéro de séquence 
+// - la valeur du temps au moment où on a transmis le segment (va être utile pour déterminer le timer des messages suivants)
+// - le nombre d'ACK reçu pour ce segment
+// - la valeur du temps au moment où on a reçu le dernier ACK (va être utile pour calculer le RTT et fixer le timer)
+
+struct segment
+{
+    int seq_number;
+    int byte_length; 
+    struct timeval trans_time;
+    int ACK_received; 
+    struct timeval last_ACK_recv_time; 
+};
+// le point de départ du segment = seq_number*sizeof(str)-1
 
 void createsocket(int *sockfd, struct sockaddr_in *servaddr, int port){
 	// Creating socket file descriptor
@@ -32,16 +50,30 @@ void createsocket(int *sockfd, struct sockaddr_in *servaddr, int port){
 	servaddr->sin_addr.s_addr = INADDR_ANY; // le serveur prend cette adresse pour être joignable sur n'importe quel interface réseau
 	servaddr->sin_port = htons(port);
 	
-	// Bind de la socket sur l'adresse du serveur 
+	// Bind de la socket : association de la socket avec la structure qui contient l'adresse et le port du serveur 
+    // pour que la socket soit associé à la couche réseau 
 	if ( bind(*sockfd, (const struct sockaddr *)servaddr, sizeof(*servaddr)) < 0 )
 	{
 		perror("bind failed");
 		exit(EXIT_FAILURE);
 	}
 }
+char *concat(char const*str1, char const*str2) {
+   size_t const l1 = strlen(str1) ;
+   size_t const l2 = strlen(str2) ;
+
+    char* result = malloc(l1 + l2 + 1);
+    if(!result) return result;
+    memcpy(result, str1, l1) ;
+    memcpy(result + l1, str2, l2 + 1);
+    return result;
+}
+
+
 
 int exchange_file(int data_socket, struct sockaddr_in data_addr){
     char buffer[MAXLINE];
+    char buffer_ack[MAXLINE];
     int n, len;
     len = sizeof(data_addr);
     // entrée dans la connexion 
@@ -50,7 +82,7 @@ int exchange_file(int data_socket, struct sockaddr_in data_addr){
     //printf("ici\n");
     printf("Client sent filename : %s\n", buffer);
     FILE *fp;
-    char str[57];
+    char str[SEGMENT_SIZE-6]; // va contenir ce qu'on lit dans le fichier
     /* opening file for reading */
     fp = fopen(buffer , "r"); // ouverture du fichier
     if(fp == NULL) {
@@ -59,38 +91,44 @@ int exchange_file(int data_socket, struct sockaddr_in data_addr){
     }
     int seq = 1; // on va écrire le numéro de séquence sur 6 bits
     int window_size = WINDOW; // taille de la fenêtre, on va la décaler dès qu'on reçoit un ACK
-    char seq_char[64];
+    //char seq_char[64];
+    char *seq_char = (char*) malloc(SEGMENT_SIZE*sizeof(char));
     int retval;
     sprintf(seq_char,"%06d",seq); // on veut écrire le numéro de séquence dans une chaîne de caractères sur 6 chiffres 
     //while( fgets(str, 64, fp)!=NULL ) { // fgets : str => la chaîne qui contient la chaine de caractère lu, 60 : nmbre max de caractère à lire, fr : stream d'où sont lus les chaînes
     int res_read; 
     int max_ack = 0; // on va regarder quel est le plus gd dernier ack recu pour savoir lesquels on a déjà reçu et qu'on ne doit donc pas traité
     int nv_ack;
-    // version avec fenêtre
-    while(window_size!=0){
-        res_read = fread(str,1,sizeof(str)-1,fp);
-        if(res_read==sizeof(str)-1){
-            /* writing content to stdout */
-            str[res_read] = '\0';
-            sprintf(seq_char,"%6d",seq);
-            strcat(seq_char,"_");
-            strcat(seq_char,str);
-            //puts(seq_char);
-            sendto(data_socket, (const char *)seq_char, strlen(seq_char), MSG_CONFIRM, (const struct sockaddr *) &data_addr, len);
-            printf("Message n°%d envoyé au client\n",seq);
-            seq++; 
-            window_size --;
-            if(window_size>1)
-                printf("Fenêtre : [%d,%d]\n",seq,seq+window_size);
-            else if(window_size==1)
-                printf("Fenêtre : [%d]\n",seq);
-            else
-                printf("Fenêtre : []\n");
+    struct stat st;
+    stat(buffer, &st);
+    size_t file_size = st.st_size;
+    printf("taille du fichier : %d\n",(int)file_size);
 
-        }
-        else{
-            break; // on est au dernier message
-        }
+
+    double nbre_seg = ceil((double)file_size/(double)(SEGMENT_SIZE-6))+1; // on arrondi à l'entier supérieur pour connaître le nombre de segments nécessaires
+    //int nbre_seg = ceil((double)17.7);
+    printf("%d segments nécessaires\n",(int)nbre_seg);
+    struct segment segments[(int)nbre_seg];
+    // version avec fenêtre
+    res_read = fread(str,1,sizeof(str),fp);
+    int nbre_octets = res_read;
+    while(res_read){
+        //printf("buffer nul\n");
+        printf("%d octets lus\n",res_read);
+        nbre_octets+= res_read;
+        //str[res_read] = '\0';
+        sprintf(seq_char,"%06d",seq);
+        // strcat et strlen ne marche qu'avec des fichier textes, à la place il faut utiliser memcpy et res_read
+        //seq_char = concat(seq_char,"_");
+        seq_char = concat(seq_char,str);
+        puts(seq_char);
+        // on ne peut pas mettre strlen(seq_char) car cela ne marche que pour les string
+        // les 7 octets en plus correspondent à "numdeseq_"
+        sendto(data_socket, (const char *)seq_char, res_read+6, MSG_CONFIRM, (const struct sockaddr *) &data_addr, len);
+        printf("Message n°%d envoyé au client\n",seq);
+        seq++; 
+        window_size --;
+        
 
         fd_set read_fds;
         FD_ZERO(&read_fds); // création du set qui va contenir la socket
@@ -102,7 +140,10 @@ int exchange_file(int data_socket, struct sockaddr_in data_addr){
             tv.tv_usec = 0; // ... millisecondes ...
             printf("fenêtre à O, on attend un ACK au maximum %d secondes sinon on retransmet\n",(int)tv.tv_sec);
              
-        }else{
+        }else if(window_size<0){
+            printf("fenêtre négative, DUPLICATE ACK (à faire) \n");
+        }
+        else{
             tv = (struct timeval){0}; 
             // initialisé à 0, on n'attend pas, on regarde et sinon on passe au message 
         }
@@ -112,89 +153,60 @@ int exchange_file(int data_socket, struct sockaddr_in data_addr){
             perror("select()");
         else if (retval){
             if(FD_ISSET(data_socket, &read_fds)!=0){
-                int recv = read(data_socket,buffer,6); // le message d'ack est de la forme ACK_ + 6 chiffres
-                buffer[recv] = '\0'; 
-                nv_ack = atoi(buffer);
+                int recv = read(data_socket,buffer_ack,sizeof(buffer_ack)); // le message d'ack est de la forme ACK_ + ...AAAA
+                //puts(buffer_ack);
+                char delim[] = "_";
+                char *seq_ptr = strtok(buffer_ack, delim);
+                //printf("seq_ptr : %s\n",seq_ptr);
+                if(strcmp(seq_ptr,"ACK")==0){
+                    //printf("ACK reçu \n");
+                    seq_ptr = strtok(NULL, delim);
+                    
+                    
+                    //puts(seq_ptr);
+                    int nv_ack = strlen(seq_ptr);
+                    // buffer[recv] = '\0'; 
+                    // nv_ack = atoi(buffer);
                 
-                printf("ACK n°%d reçu du client\n", nv_ack);
-                if(nv_ack>max_ack){
-                    window_size += nv_ack-max_ack; //((nv_ack-max_ack)+window_size>WINDOW) ? 5 : 
-                    max_ack = nv_ack;
-                    printf("Taille de fenêtre : %d\n",window_size);
-                }else{
-                    printf("message déjà Ack'd");
-                    if(tv.tv_sec!=0){
-                        printf("Retransmission");
+                    printf("ACK n°%d reçu du client\n", nv_ack);
+                    if(nv_ack>max_ack){
+                        window_size += nv_ack-max_ack; //((nv_ack-max_ack)+window_size>WINDOW) ? 5 : 
+                        max_ack = nv_ack;
+                        printf("Taille de fenêtre : %d\n",window_size);
+                    }else{
+                        printf("DUPLICATE ACK ou OUT OF ORDER\n");
+                        // if(tv.tv_sec!=0){
+                        //     //return 1;
+                        //     printf("Retransmission à faire (duplicate ACK) \n");
+                        // }
                     }
                 }
                 
+        
             }
         }
-        else
+        else{
             printf("Aucun ACK reçu.\n");
-    } 
-    // Version sans fenêtre :
-    // while(read==sizeof(str)-1){ // tant qu'on arrive à lire les 56 octets
-    
-    //     /* writing content to stdout */
-    //     str[read] = '\0';
-    //     //puts(str);
-    //     // on va écrire le texte vers le client 
-    //     sprintf(seq_char,"%6d",seq);
-    //     strcat(seq_char,"_");
-    //     strcat(seq_char,str);
-    //     //printf("seq : %s\n",seq_char);
-    //     sendto(data_socket, (const char *)seq_char, strlen(seq_char), MSG_CONFIRM, (const struct sockaddr *) &data_addr, len);
-    //     //bzero(str,sizeof(str));
-    //     n = recvfrom(data_socket, (char *)buffer, MAXLINE, MSG_WAITALL, ( struct sockaddr *) &data_addr, &len);
-    //     buffer[n] = '\0'; 
-    //     printf("Client : %s\n", buffer);
-    //     seq++;
-    //     bzero(str,sizeof(str));
-    //     read = fread(str,1,sizeof(str)-1,fp);
-    //     //printf("read : %d\n",read);
-    //     //puts(str);
-    // }
-    str[res_read] = '\0';
-    sprintf(seq_char,"%6d",seq);
-    strcat(seq_char,"_");
-    char last_message[res_read];
-    strcpy(last_message,str);
-    strcat(seq_char,last_message);
-    printf("Serveur (dernier message) : %s\n",seq_char);
-    sendto(data_socket, (const char *)seq_char, strlen(seq_char), MSG_CONFIRM, (const struct sockaddr *) &data_addr, len);
-    //bzero(str,sizeof(str));
-    // ici il faut attendre les derniers ACK
-    n = recvfrom(data_socket, (char *)buffer, MAXLINE, MSG_WAITALL, ( struct sockaddr *) &data_addr, &len);
-    buffer[n] = '\0'; 
-    printf("Client : %s\n", buffer);
-    //puts(str);
-    strcpy(str,"EOF");
-    sendto(data_socket, (const char *)str, strlen(str), MSG_CONFIRM, (const struct sockaddr *) &data_addr, len);
+        }
+
+        if(window_size>1)
+            printf("Fenêtre : [%d,%d]\n",seq,seq+window_size);
+        else if(window_size==1)
+            printf("Fenêtre : [%d]\n",seq);
+        else
+            printf("Fenêtre : []\n");
+        res_read = fread(str,1,sizeof(str),fp);
+    }
+    char *eof = "EOF";
+    sendto(data_socket, (const char *)eof, strlen(eof), MSG_CONFIRM, (const struct sockaddr *) &data_addr, len);
     fclose(fp);
     close(data_socket);
+    printf("fichier de %d octets\n",(int)file_size);
+    printf("%d octets lus\n",(int)nbre_octets);
+    printf("%d : dernier ACK\n",max_ack);
     return 1;
 
 }
-
-
-// void handle_client(int data_socket, struct sockaddr_in data_addr){
-//     char buffer[MAXLINE];
-//     bzero(buffer,sizeof(buffer));
-//     int len = sizeof(data_addr);
-//     while(1){
-//         int n = recvfrom(data_socket, (char *)buffer, MAXLINE, MSG_WAITALL, ( struct sockaddr *) &data_addr, &len);
-//         buffer[n] = '\0'; 
-// 	    printf("Client : %s\n", buffer);
-//         char message[64] = "ACK_"; // on va concaténer l'ACK avec le numéro du dernier message reçu en continu
-//         // on suppose que le message est son propre numéro
-//         // char port[5];
-//         // sprintf(port, "%d", port2);
-//         strcat(message,buffer);
-//         printf("message : %s\n", message);
-//         sendto(data_socket, (const char *)message, strlen(message), MSG_CONFIRM, (const struct sockaddr *) &data_addr, len);
-//     }
-// }
 
 
 int twh_serv(int sockfd, struct sockaddr_in cliaddr){
@@ -228,7 +240,6 @@ int twh_serv(int sockfd, struct sockaddr_in cliaddr){
             if(ex==1){
                 return 1; 
             }
-            //nbr_sock ++;
             // Test fork : 
             // int child_pid = fork();
             // if (child_pid == 0){
